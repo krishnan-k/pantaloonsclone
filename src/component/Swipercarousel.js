42b8beb1a2416ebdea88328438410b2995c7175d
krishnan <krishnank281099@gmail.com>
2024-07-10 21:09:05 +0530

    swiper

--- src/component/Swipercarousel.js
+++ src/component/Swipercarousel.js
@@ -1,6 +1,11 @@
 import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './swipercarousel.css'
 import slideOne from '../image/slide-1.png';
 import slideTwo from '../image/slide-2.png';
 import slideThree from '../image/slide-3.png';
@@ -10,14 +15,16 @@ import slideFive from '../image/slide-5.png';
 
 
 export const Swipercarousel = () => {
+  const imaClassName = 'image_class';
   return (
     <div className="SwiperCarousel">
-      <Swiper className="mySwiper">
-        <SwiperSlide><img src={slideOne}/></SwiperSlide>
-        <SwiperSlide><img src={slideTwo}/></SwiperSlide>
-        <SwiperSlide><img src={slideThree}/></SwiperSlide>
-        <SwiperSlide><img src={slideFour}/></SwiperSlide>
-        <SwiperSlide><img src={slideFive}/></SwiperSlide>
+      <Swiper pagination ={true} modules={[Pagination,Navigation]} navigation = {true} className="mySwiper">
+        <SwiperSlide><img className={imaClassName} src={slideOne} alt='slider-image' /></SwiperSlide>
+        <SwiperSlide><img className={imaClassName} src={slideTwo} alt='slider-image' /></SwiperSlide>
+        <SwiperSlide><img className={imaClassName} src={slideThree} alt='slider-image' /></SwiperSlide>
+        <SwiperSlide><img className={imaClassName} src={slideFour} alt='slider-image' /></SwiperSlide>
+        <SwiperSlide><img className={imaClassName} src={slideFive} alt='slider-image' /></SwiperSlide>
+
       </Swiper>
     </div>
   );
