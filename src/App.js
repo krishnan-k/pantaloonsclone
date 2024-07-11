a5c4353d55b943786243b971672ff1206490621f
krishnan <krishnank281099@gmail.com>
2024-07-11 13:53:07 +0530

    card changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navbar } from "./component/Navbar";
 import {Swipercarousel} from "./component/Swipercarousel"
+import { Cardcollection } from "./component/Cardcollection";
 function App() {
   return (
     <div>
       <Navbar/>
       <Swipercarousel/>
+      <Cardcollection/>
     </div>
   );
 }
--- src/component/Swipercarousel.js
+++ src/component/Swipercarousel.js
@@ -11,6 +11,7 @@ import slideTwo from '../image/slide-2.png';
 import slideThree from '../image/slide-3.png';
 import slideFour from '../image/slide-4.png';
 import slideFive from '../image/slide-5.png';
+import slideSix from '../image/slide-6.jpg';
 // Import Swiper styles
 
 
@@ -22,12 +23,24 @@ export const Swipercarousel = () => {
   return (
     <div className="SwiperCarousel">
       <Swiper pagination={pagination} navigation = {true}  modules={[Navigation,Pagination]} className="mySwiper">
-        <SwiperSlide><img className={imaClassName} src={slideOne} alt='slider-image' /></SwiperSlide>
-        <SwiperSlide><img className={imaClassName} src={slideTwo} alt='slider-image' /></SwiperSlide>
-        <SwiperSlide><img className={imaClassName} src={slideThree} alt='slider-image' /></SwiperSlide>
-        <SwiperSlide><img className={imaClassName} src={slideFour} alt='slider-image' /></SwiperSlide>
-        <SwiperSlide><img className={imaClassName} src={slideFive} alt='slider-image' /></SwiperSlide>
-
+      <SwiperSlide><img className={imaClassName} src={slideSix} alt='slider-image' />
+        <a class="text-decoration-none text-dark mt-3 mb-3" href="">Pantaloons Sale is here! Get Upto 60% Off + Extra Rs.1500/- Off* on order value of Rs.5999/-, Code: SALE1500.</a>
+        </SwiperSlide>
+        <SwiperSlide><img className={imaClassName} src={slideOne} alt='slider-image' />
+        <a class="text-decoration-none text-dark mt-3 mb-3" href="">Pantaloons Sale is here! Get Upto 60% Off + Extra Rs.1500/- Off* on order value of Rs.5999/-, Code: SALE1500.</a>
+        </SwiperSlide>
+        <SwiperSlide><img className={imaClassName} src={slideTwo} alt='slider-image' />
+        <a class="text-decoration-none text-dark mt-3 mb-3" href="">Pantaloons Sale is here! Get Upto 60% Off + Extra Rs.1500/- Off* on order value of Rs.5999/-, Code: SALE1500.</a>
+        </SwiperSlide>
+        <SwiperSlide><img className={imaClassName} src={slideThree} alt='slider-image' />
+        <a class="text-decoration-none text-dark mt-3 mb-3" href="">Pantaloons Sale is here! Get Upto 60% Off + Extra Rs.1500/- Off* on order value of Rs.5999/-, Code: SALE1500.</a>
+        </SwiperSlide>
+        <SwiperSlide><img className={imaClassName} src={slideFour} alt='slider-image' />
+        <a class="text-decoration-none text-dark mt-3 mb-3" href="">Pantaloons Sale is here! Get Upto 60% Off + Extra Rs.1500/- Off* on order value of Rs.5999/-, Code: SALE1500.</a>
+        </SwiperSlide>
+        <SwiperSlide><img className={imaClassName} src={slideFive} alt='slider-image' />
+        <a class="text-decoration-none text-dark mt-3 mb-3" href="">Pantaloons Sale is here! Get Upto 60% Off + Extra Rs.1500/- Off* on order value of Rs.5999/-, Code: SALE1500.</a>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
