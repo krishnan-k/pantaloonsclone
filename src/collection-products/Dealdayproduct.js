a16f831d552365ef2465d376d7995f8a1ff7fa14
krishnan <krishnank281099@gmail.com>
2024-07-24 07:21:23 +0530

    24/07/2024

--- src/collection-products/Dealdayproduct.js
+++ src/collection-products/Dealdayproduct.js
@@ -14,72 +14,81 @@ const dealDayProduct = [
         image: product27,
         title: "indus route",
         description: "Blue Solid Casual Men Regular Fit Kurtas",
-        price: "824",
-        comparePrice:"1099"
+        price: 824,
+        comparePrice: 1099,
+        quantity: 50
     },
     {
         id: 2,
         image: product28,
         title: "rangmanch",
         description: "Peach Printed Athleisure Half Sleeves Round Neck Women Regular",
-        price: "719",
-        comparePrice:"899"
+        price: 719,
+        comparePrice:899,
+        quantity: 50
     },
     {
         id: 3,
         image: product29,
         title: "people",
         description: "Medium Blue Textured Casual Half Sleeves Polo Collar Men Slim",
-        price: "655",
-        comparePrice:"799"
+        price: 655,
+        comparePrice:799,
+        quantity: 50
     },
     {
         id: 4,
         image: product30,
         title: "bare denim",
         description: "White Printed Party Full Sleeves Shirt Collar Men Slim Fit",
-        price: "594",
-        comparePrice:"699"
+        price: 597,
+        comparePrice:699,
+        quantity: 50
     },
     {
         id: 5,
         image: product31,
         title: "rangmanch",
         description: "Yellow Print Casual V Neck 3/4th Sleeves Knee Length Women",
-        price: "629",
-        comparePrice:"899"
+        price: 629,
+        comparePrice:899,
+        quantity: 50
     },
     {
         id: 6,
         image: product32,
         title: "Street 808",
         description: "Medium Grey Solid Full Length Casual Men Regular Fit Joggers",
-        price: "749",
-        comparePrice:"999"
+        price: 749,
+        comparePrice:999,
+        quantity: 50
     },
     {
         id: 7,
         image: product33,
         title: "Ajile",
         description: "Pink Printed Full Length Mid Rise Casual Women Jogger Track Pants",
-        price: "679",
-        comparePrice:"799"
+        price: 679,
+        comparePrice:799,
+        quantity: 50
     },
     {
         id: 8,
         image: product34,
         title: "Marigold Lane",
         description: "Yellow Embroidered Ethnic 3/4th Sleeves Square Neck Women Flared Fit  Kurta Gharara Dupatta Set",
-        price: "629",
-        comparePrice:"699"
+        price: 629,
+        comparePrice:699,
+        quantity: 50
     },
     {
         id: 9,
         image: product35,
         title: "SF Jeans",
         description: "Medium Blue Printed Casual Full Sleeves Shirt Collar Men Slim Fit Casual Shirts",
-        price: "824",
-        comparePrice:"1099"
+        price: 824,
+        comparePrice:1099,
+        quantity: 50
     }
 ]
 export default dealDayProduct
